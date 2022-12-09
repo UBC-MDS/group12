eddc143a26658e5ccdbab50c00d7925cd078ade3
kellywujy <112680707+kellywujy@users.noreply.github.com>
2022-12-08 16:14:30 -0800

    Added an empty string between usage and option

--- src/download_file.py
+++ src/download_file.py
@@ -6,6 +6,7 @@ The databases will be downloaded to the out_file path specified.
 In this project the data is downloaded into the data/raw folder.
 
 Usage: download_file.py --url=<url> --out_file=<out_file>
+
 Options:
 --url=<url>              URL from where to download the data (must be in standard csv format)
 --out_file=<out_file>    Path (including filename) of where to locally write the file
